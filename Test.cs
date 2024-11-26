4a865469a80ae061150d411b87ea95da8fe184dc
Enko <me@enkophi.com>
2024-11-27 00:41:15 +0800

    physics test

--- Test.cs
+++ Test.cs
@@ -1,16 +1,15 @@
 using Godot;
 
-namespace test;
-
-public partial class Test : Node
+namespace test
 {
-	[Export]
-	public Node3D node;
+    public partial class Test : Node
+    {
+        [Export]
+        public Node3D node;
 
-	public override void _Ready()
-	{
-		GD.Print("Hello, world from csharp!");
-		GD.Print(node);
-		node.Position += new Vector3(1, 1, 1);
-	}
+        public override void _Ready()
+        {
+            GD.Print("Hello, world from csharp!");
+        }
+    }
 }
