dc456a4330854f417661c2b42f223bb7fd023e94
Enko <me@enkophi.com>
2024-11-28 17:05:44 +0800

    test change scene

---
+++ MainMenu.cs
@@ -0,0 +1,50 @@
+﻿using Godot;
+
+namespace test;
+
+public partial class MainMenu : Control
+{
+    [Export] public BaseButton newGameButton;
+    [Export] public BaseButton loadGameButton;
+    [Export] public BaseButton settingsButton;
+    [Export] public BaseButton quitButton;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        newGameButton.Pressed += OnNewGameButtonPressed;
+        loadGameButton.Pressed += OnLoadGameButtonPressed;
+        settingsButton.Pressed += OnSettingsButtonPressed;
+        quitButton.Pressed += OnQuitButtonPressed;
+    }
+
+    public override void _ExitTree()
+    {
+        base._ExitTree();
+        newGameButton.Pressed -= OnNewGameButtonPressed;
+        loadGameButton.Pressed -= OnLoadGameButtonPressed;
+        settingsButton.Pressed -= OnSettingsButtonPressed;
+        quitButton.Pressed -= OnQuitButtonPressed;
+    }
+
+    private void OnNewGameButtonPressed()
+    {
+        // Load the game scene
+        GetTree().ChangeSceneToFile("res://Game.tscn");
+        // Destroy this
+        QueueFree();
+    }
+
+    private void OnLoadGameButtonPressed()
+    {
+    }
+
+    private void OnSettingsButtonPressed()
+    {
+    }
+
+    private void OnQuitButtonPressed()
+    {
+        GetTree().Quit();
+    }
+}
